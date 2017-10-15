19d9361c2f8b94399df1af8c2098e69f2b9f8efa
Asaf Silman <asafsilman@gmail.com>
2017-10-15 15:13:34 +0800

    Completed Step 2

--- execute.c
+++ execute.c
@@ -14,6 +14,31 @@
 //  THAT IT HOLDS, RETURNING THE APPROPRIATE EXIT-STATUS.
 //  READ print_shellcmd0() IN globals.c TO SEE HOW TO TRAVERSE THE COMMAND-TREE
 
+void runfrompath(char **argv)
+{
+    char *path_p = getenv("PATH");
+    const char s[2] = ":"; //seperator
+    char *token;
+    char command_buffer[256]; // Buffer to hold command
+
+    char *command = calloc(strlen(argv[0])+2, sizeof(char));
+    strcat(command, "/"); 
+    strcat(command, argv[0]); 
+
+    token = strtok(path_p, s);
+    // iterate through PATH variable
+    while(token != NULL)
+    {
+        strcpy(command_buffer, token);
+        strcat(command_buffer, command)
+        execv(command_buffer, argv); //if this works, the process image is replaced. execution of loop will stop
+
+        token = strtok(NULL, s);
+    }
+
+    free(command);
+}
+
 int execute_shellcmd(SHELLCMD *t)
 {
     int  exitstatus;
@@ -26,8 +51,14 @@ int execute_shellcmd(SHELLCMD *t)
         pid_t  pid = fork();
         switch (pid){
             case 0 : // child process
-                execv(t->argv[0], t->argv); // attempt to start process
-                
+                if (t->argv[0][0] == '/'){
+                    // Path Given
+                    execv(t->argv[0], t->argv); // attempt to start process
+                }
+                else{
+                    // Search path for command
+                    runfrompath(t->argv);
+                }
                 // If execv failed, and process image did not change
                 fprintf(stderr, "%s is not a valid command\n", t->argv[0]);
                 exit(EXIT_FAILURE);
