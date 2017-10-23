9ae992a894ece3e31bc96c360e69114eb1e9b552
Madeleine Lim <21708238@student.uwa.edu.au>
2017-10-23 20:06:24 +0800

    Moved Commands into new file structure, created util.c + util.h for commonly used functions

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
-myshell: parser.o execute.o	globals.o myshell.h
-	cc -std=c99 -Wall -Werror -pedantic -o myshell myshell.c parser.o execute.o globals.o
+myshell: myshell.c parser.o execute.o globals.o commands.o util.o myshell.h
+	cc -std=c99 -Wall -Werror -pedantic -o myshell myshell.c parser.o execute.o globals.o util.o commands.o
+
+commands.o: commands.c util.h myshell.h
+	cc -std=c99 -Wall -Werror -pedantic -c -o commands.o commands.c
+
+util.o: util.c util.h myshell.h
+	cc -std=c99 -Wall -Werror -pedantic -c -o util.o util.c
 
 parser.o: parser.c myshell.h
 	cc -std=c99 -Wall -Werror -pedantic -c -o parser.o parser.c
@@ -11,4 +17,4 @@ globals.o: globals.c myshell.h
 	cc -std=c99 -Wall -Werror -pedantic -c -o globals.o globals.c
 
 clean:
-	rm -f myshell parser.o execute.o globals.o
+	rm -f myshell parser.o execute.o globals.o util.o commands.o
---
+++ util.c
@@ -0,0 +1,54 @@
+#include "myshell.h"
+#include "util.h"
+
+void run_cmd(int *exitstatus, SHELLCMD *t)
+{
+    pid_t  pid = fork();
+    switch (pid){
+        case 0 : // child process
+            if (t->argv[0][0] == '/'){
+                // Path Given
+                execv(t->argv[0], t->argv); // attempt to start process
+            }
+            else{
+                // Search path for command
+                runfrompath(t->argv);
+            }
+            // If execv failed, and process image did not change
+            fprintf(stderr, "%s is not a valid command\n", t->argv[0]);
+            exit(EXIT_FAILURE);
+            break;
+        case -1 : //fork failed
+            *exitstatus	= EXIT_FAILURE;
+            break;
+        default : // parent process
+            wait( exitstatus ); //exit status is written once child terminates
+            // CAN PRINT THE EXIT CODE HERE
+            break;
+    }
+}
+
+void runfrompath(char **argv)
+{
+    char *path_p = getenv("PATH");
+    const char s[2] = ":"; //seperator
+    char *token;
+    char command_buffer[256]; // Buffer to hold command
+
+    char *command = calloc(strlen(argv[0])+2, sizeof(char));
+    strcat(command, "/"); 
+    strcat(command, argv[0]); 
+
+    token = strtok(path_p, s);
+    // iterate through PATH variable
+    while(token != NULL)
+    {
+        strcpy(command_buffer, token);
+        strcat(command_buffer, command);
+        execv(command_buffer, argv); //if this works, the process image is replaced. execution of loop will stop
+        token = strtok(NULL, s);
+    }
+
+    free(command);
+}
+
